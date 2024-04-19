af85a8b2e88c427f50878797d093f6248bfaaea3
Farsh sabz <101440241@student.swin.edu.au>
2024-04-19 16:56:09 +1000

    Modified the CS

--- Login/LoginPage.xaml.cs
+++ Login/LoginPage.xaml.cs
@@ -1,14 +1,65 @@
 namespace OnboardingSystem;
 
 public partial class LoginPage : ContentPage
-{
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+{   private List<string> loggedInUsernames = new List<string>(); 
+    private string _username;
+    private string _password;
+    private bool _hasError;
+    private string _errorMessage;
 
-	private async void ToMain(object sender, EventArgs e) //on click button
-	{
-		await Shell.Current.GoToAsync($"//{nameof(MainPage)}"); //redirect to main page
-	}
+    public string Username
+    {
+        get => _username;
+        set
+        {
+            _username = value;
+            OnPropertyChanged(nameof(Username));
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set
+        {
+            _password = value;
+            OnPropertyChanged(nameof(Password));
+        }
+    }
+
+    public bool HasError
+    {
+        get => _hasError;
+        set
+        {
+            _hasError = value;
+            OnPropertyChanged(nameof(HasError));
+        }
+    }
+
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        set
+        {
+            _errorMessage = value;
+            OnPropertyChanged(nameof(ErrorMessage));
+        }
+    }
+
+    public LoginPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    private async void ToMain(object sender, EventArgs e)
+    {
+      
+			 loggedInUsernames.Add(Username);
+            // Authentication successful
+            await Shell.Current.GoToAsync($"//{nameof(MainPage)}");
+       
+    }
+
 }
