0ec3d8dd1f1873b9e802d02c969219b447cc5455
Kylzia <kylziabell@gmail.com>
2024-09-30 18:34:10 +1000

    Profile Design

--- Profile/ProfilePage.xaml.cs
+++ Profile/ProfilePage.xaml.cs
@@ -1,11 +1,43 @@
 using System;
+using System.ComponentModel;
 
 namespace OnboardingSystem;
 
-public partial class ProfilePage : ContentPage
+public partial class ProfilePage : ContentPage, INotifyPropertyChanged
 {
+	private string _username = "";
+	private string _password = "";
+	private string _firstName = "";
+	private string _lastName = "";
+	private string _role = "";
+	private string _phone = "";
+
 	public ProfilePage()
 	{
 		InitializeComponent();
+	}
+
+	public string Username
+	{
+		get => _username;
+		set => _username = value;
+	}
+
+	public string Password
+	{
+		get => _password;
+		set => _password = value;
+	}
+
+	public string FirstName
+	{
+		get => _firstName;
+		set => _firstName = value;
+	}
+
+	public string LastName
+	{
+		get => _lastName;
+		set => _lastName = value;
 	}
 }
