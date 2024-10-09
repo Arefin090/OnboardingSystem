05dbf1c74fb8c040d2009a228c410b60af19f837
nhatjollie <nguyendnhat12@gmail.com>
2024-10-09 17:45:33 +1100

    Removed Reload
    Fixed

--- Dashboard/DashboardPage.xaml.cs
+++ Dashboard/DashboardPage.xaml.cs
@@ -251,11 +251,6 @@ public partial class DashboardPage : ContentPage
         }
     }
 
-    // private void OnLoadClicked(object sender, EventArgs e)
-    // {
-    //     LoadConfig();
-    // }
-
     public class ChartConfig
     {
         public string? BarChartTable { get; set; }
